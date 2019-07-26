0d4e52f89000472f39dc312a841ed770c232ab9e
Donne Dylan <dylan_donne@ens.univ-artois.fr>
2019-07-26 18:49:45 +0200

    Implementing JDBC test

--- src/fr/killax/app/App.java
+++ src/fr/killax/app/App.java
@@ -3,18 +3,23 @@ package fr.killax.app;
 import java.util.Timer;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import fr.killax.app.data.MySQLClient;
 import fr.killax.app.data.Product;
 import fr.killax.app.data.User;
-import fr.killax.logger.Logger;
+import fr.killax.app.data.mysql.Column;
+import fr.killax.app.data.mysql.Tuple;
 
 public class App {
 
+	private static MySQLClient mysql;
 	private static CopyOnWriteArrayList<Product> products = new CopyOnWriteArrayList<Product>();
-
-	public static Logger logger;
 	
 	public static void main(String[] args) {
-		products.add(new Product("https://www.amazon.fr/dp/B01N5OPMJW/ref=cm_sw_r_tw_dp_U_x_WgCoDb7W2987S", 15.00, new User("M", "Colique", "Al", "contact@dylan-donne.fr")));
+		MySQL();
+		User user = new User("M", "Colique", "Al", "karyumgames@gmail.com");
+		String link = "https://www.amazon.fr/Fossil-FTW4017-Montre-Connect%C3%A9e/dp/B07H57VJ5W/ref=sr_1_11?__mk_fr_FR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=Fossil&qid=1564149880&s=gateway&sr=8-11";
+		
+		products.add(new Product(link, 99999.00, user));
 		runChecker();
 	}
 	
@@ -25,5 +30,29 @@ public class App {
 
 	public static CopyOnWriteArrayList<Product> getProducts() {
 		return products;
+	}
+	
+	private static void MySQL() {
+		try {
+			mysql = new MySQLClient("localhost", "root", "", 3306);
+			mysql.connect();
+			mysql.setDatabase("test");
+			mysql.createTable("test", 
+				new Column("id", "INT AUTO_INCREMENT PRIMARY KEY"),
+				new Column("name", "VARCHAR(255)")
+			);
+			mysql.insert("test", new Column[] {
+					new Column("name", "VARCHAR(255)")
+				}
+				,new Tuple[] {
+					new Tuple("Killax"),
+					new Tuple("Rokem")
+				}
+			);
+			System.out.println(String.format("Result of Selection : %s", mysql.selectAllToMap("test", "id").toString()));
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
